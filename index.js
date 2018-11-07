e6bad346c31d74336251cc36a8a2d00b6c875681
Carlo Bruno <carlofbruno@gmail.com>
2018-11-07 06:45:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,22 @@ function subtract(a,b) {
   return a - b;
 }
 
+function multiply(a,b) {
+  return a * b;
+}
+
+function divide(a,b) {
+  return a / b;
+}
+
+function inc(n) {
+  return n+=1;
+}
+
+function dec(n) {
+  return n-=1;
+}
+
+function makeInt(n) {
+  return parseInt(n,10);
+}
